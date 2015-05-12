de1031f6368afbcd60c0bce053e65aa7bac773b7
Anthony Mace <amace93@gmail.com>
2015-05-12 14:30:19 -0700

    added classDetailsVC

---
+++ ClassDetailsVC.swift
@@ -0,0 +1,41 @@
+//
+//  ClassDetailsVC.swift
+//  Median
+//
+//  Created by Anthony Mace on 5/12/15.
+//  Copyright (c) 2015 Mace. All rights reserved.
+//
+
+import UIKit
+
+class ClassDetailsVC: UIViewController {
+    //MARK: - Properties
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var sectionField: UITextField!
+    @IBOutlet weak var daysField: UITextField!
+    @IBOutlet weak var timeField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
