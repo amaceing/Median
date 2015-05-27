ab4d45cf4182674332f747b1f3c4c53a51551a1c
Anthony Mace <amace93@gmail.com>
2015-05-27 14:34:25 -0700

    created ClassCircle file

---
+++ ClassCircle.swift
@@ -0,0 +1,21 @@
+//
+//  ClassCircle.swift
+//  Median
+//
+//  Created by Anthony Mace on 5/27/15.
+//  Copyright (c) 2015 Mace. All rights reserved.
+//
+
+import UIKit
+
+class ClassCircle: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
