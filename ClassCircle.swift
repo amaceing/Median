4bb656ea01e37505fc43ab7f38da37d2cf4c955c
Anthony Mace <amace93@gmail.com>
2015-05-27 14:37:35 -0700

    started implementing ClassCircle methods

--- ClassCircle.swift
+++ ClassCircle.swift
@@ -9,13 +9,22 @@
 import UIKit
 
 class ClassCircle: UIView {
+    var grade: Double = 0.0
+    
+    override init(frame: CGRect) {
+        super.init(frame: CGRect())
+        self.opaque = false
+    }
+        
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
-    /*
+    
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func drawRect(rect: CGRect) {
         // Drawing code
     }
-    */
-
 }
