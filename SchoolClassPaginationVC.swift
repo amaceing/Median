693136384c58811b6c5f99dd35e62117b423376b
Anthony Mace <amace93@gmail.com>
2015-06-18 10:05:17 -0700

    implemented setting up navBar

--- SchoolClassPaginationVC.swift
+++ SchoolClassPaginationVC.swift
@@ -14,6 +14,18 @@ class SchoolClassPaginationVC: UIViewController, UIPageViewControllerDelegate, U
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setUpNavBar()
+    }
+    
+    func setUpNavBar() {
+        var title = " "
+        let navBarBlue = UIColor(red: 30/255.0, green: 178/255.0, blue: 192/255.0, alpha: 1)
+        self.navigationItem.title = title.determineSeasonAndYear()
+        self.navigationController?.navigationBar.barTintColor = navBarBlue
+        self.navigationController?.navigationBar.translucent = false
+        self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
+        self.navigationController?.navigationBar.barStyle = UIBarStyle.BlackTranslucent
+        self.navigationController?.navigationBar.titleTextAttributes = [ NSFontAttributeName: UIFont(name: "Lato-Regular", size: 24)!]
     }
 
     override func didReceiveMemoryWarning() {
--- SchoolClassVC.swift
+++ SchoolClassVC.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class SchoolClassVC: UIViewController {
+    //MARK: - Properties
+    var schoolClass: SchoolClass?
+    var index: NSInteger?
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
