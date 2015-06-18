db490ba1a1e8cce5ba9087da564dace1ae33edb6
Anthony Mace <amace93@gmail.com>
2015-06-18 14:06:22 -0700

    implemented class details label in SchoolClassVC

--- SchoolClassVC.swift
+++ SchoolClassVC.swift
@@ -13,7 +13,11 @@ class SchoolClassVC: UIViewController {
     var schoolClass: SchoolClass
     var index: NSInteger
     @IBOutlet weak var classLabel: UILabel!
+    @IBOutlet weak var gradeLabel: UILabel!
+    @IBOutlet weak var detailsLabel: UILabel!
     
+    
+    //MARK: - View loading methods
     init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?, index: NSInteger, schoolClass: SchoolClass) {
         self.schoolClass = schoolClass
         self.index = index
@@ -35,11 +39,45 @@ class SchoolClassVC: UIViewController {
 
         // Do any additional setup after loading the view.
         self.classLabel.text = self.schoolClass.name
+        self.gradeLabelSetUp()
+        self.detailLabelsSetUp()
+    }
+    
+    func gradeLabelSetUp() {
+        let textColor: UIColor
+        if (self.schoolClass.grade >= 85) {
+            textColor = UIColor(red:124/255.0, green:209/255.0, blue:30/255.0 ,alpha:1)
+        } else if (self.schoolClass.grade >= 70) {
+            textColor = UIColor(red:243/255.0, green:172/255.0, blue:54/255.0 ,alpha:1)
+        } else {
+            textColor = UIColor(red:233/255.0, green:69/255.0, blue:89/255.0 ,alpha:1)
+        }
+        self.gradeLabel.textColor = textColor
+        self.gradeLabel.text = self.schoolClass.grade.description
+    }
+    
+    func detailLabelsSetUp() {
+        let details = String(format: "%    @     ●    %@    ●    %@    ", self.schoolClass.section, self.schoolClass.daysOfWeek, self.schoolClass.timeOfDay)
+        self.detailsLabel.text = details
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func gradeTextSetUp() {
+        var gradeLabelsRect = self.determineGradeLabelsFrame()
+    }
+    
+    func determineGradeLabelsFrame() -> CGRect {
+        var gradeLabelsRect: CGRect
+        if (self.schoolClass.grade >= 99.95) {
+            gradeLabelsRect = CGRectMake(57, 84, 280, 80);
+        } else {
+            gradeLabelsRect = CGRectMake(22, 84, 280, 80);
+        }
+        return gradeLabelsRect
     }
     
 
