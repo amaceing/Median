923873e28913e2c4ade6e25235be136ac6bcd9a8
Anthony Mace <amace93@gmail.com>
2015-06-19 09:32:21 -0700

    added files for AssignCatCell

---
+++ AssignmentCategoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  AssignmentCategoryCell.swift
+//  Median
+//
+//  Created by Anthony Mace on 6/19/15.
+//  Copyright (c) 2015 Mace. All rights reserved.
+//
+
+import UIKit
+
+class AssignmentCategoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
