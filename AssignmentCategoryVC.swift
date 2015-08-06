7fed707ae6f8cefb820a20404c52aa2dd6d23785
Anthony Mace <amace93@gmail.com>
2015-08-05 19:48:10 -0700

    added initializers for AssignCatVC

--- AssignmentCategoryVC.swift
+++ AssignmentCategoryVC.swift
@@ -16,6 +16,15 @@ class AssignmentCategoryVC: UIViewController, UITableViewDelegate, UITableViewDa
     @IBOutlet weak var tableView: UITableView!
     
     //MARK: - View Loading Methods
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        self.category = AssignmentCategory()
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,7 +56,7 @@ class AssignmentCategoryVC: UIViewController, UITableViewDelegate, UITableViewDa
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        self.category.assignmentList.count
+        return self.category.assignmentList.count
     }
     
     func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
